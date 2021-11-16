94c3863a5d184768ebe1651ec13ed0410116ca1c
Irvin Lee <irvin.lee.work@gmail.com>
2021-11-15 22:33:20 -0500

    final commit

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -9,8 +9,10 @@ class PokeFetch extends Component {
       pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
+      seconds: 10
     }
   }
+
 
   fetchPokemon() {
     let min = Math.ceil(1);
@@ -24,19 +26,32 @@ class PokeFetch extends Component {
           pokeInfo: res,
           pokeSprite: res.sprites.front_default,
           pokeName: res.species.name,
+          seconds: 10
         })
       })
       .catch((err) => console.log(err))
   }
 
+  timer = () => {
+    let countdown = setInterval(() => {
+      if (this.state.seconds > 0) {
+        this.setState({
+          seconds: this.state.seconds - 1
+        })
+      } else if (this.state.seconds == 0) {
+        clearInterval(countdown)
+      }
+    }, 1000)
+  };
+
   render() {
     return (
       <div className={'wrapper'}>
-        <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-        <h1 className={'timer'} >Timer Display</h1>
+        <button className={'start'} onClick={() => {this.fetchPokemon(); this.timer()}}>Start!</button>
+        <h1 className={'timer'} >{this.state.seconds}</h1>
         <div className={'pokeWrap'}>
-          <img className={'pokeImg'} src={this.state.pokeSprite} />
-          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
+          <img className={'pokeImg'} src={this.state.pokeSprite} style={this.state.seconds == 0 ? {filter: "brightness(100%)"} : {filter: "brightness(0%)"}} />
+          <h1 className={'pokeName'} style={this.state.seconds == 0 ? {opacity: "1"} : {opacity: "0"}}>{this.state.pokeName}</h1>
         </div>
       </div>
     )
